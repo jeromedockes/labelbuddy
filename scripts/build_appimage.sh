8fb94d1492a3e2dc1812750b2caf28008688444a
Jerome Dockes <jerome@dockes.org>
2023-01-05 19:34:07 -0300

    add version number in artifact file names

--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -23,8 +23,13 @@ chmod u+x linuxdeploy-*.AppImage
 ./linuxdeploy-x86_64.AppImage --appdir AppDir
 ./linuxdeploy-x86_64.AppImage --appdir AppDir --plugin qt --output appimage
 
-chmod u+x labelbuddy*.AppImage
-cp labelbuddy*.AppImage "$TARGET_DIR"
+APPIMAGE=$(find -maxdepth 1 -name 'labelbuddy*.AppImage' -printf '%f')
+LABELBUDDY_VERSION=$(cat "$REPO_DIR/data/VERSION.txt")
+APPIMAGE_WITH_VERSION=$(echo "$APPIMAGE" | sed "s/^labelbuddy/labelbuddy-$LABELBUDDY_VERSION/")
+mv "$APPIMAGE" "$APPIMAGE_WITH_VERSION"
+chmod u+x "$APPIMAGE_WITH_VERSION"
+md5sum "$APPIMAGE_WITH_VERSION" > "$APPIMAGE_WITH_VERSION.md5"
+cp $APPIMAGE_WITH_VERSION* "$TARGET_DIR"
 cp -r AppDir "$TARGET_DIR/AppDir"
 
 echo "AppImage in $TARGET_DIR"
